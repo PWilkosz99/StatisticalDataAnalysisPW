b8833e3e26509f41478d8cd2d88c5c1b580b7d40
Elomelo 320 <szymongooogle@gmail.com>
2021-06-03 15:38:53 +0200

    zamiana NA oraz zmiana kodowania

--- ProjektZaliczenie/WykresKorelacja.r
+++ ProjektZaliczenie/WykresKorelacja.r
@@ -1,6 +1,11 @@
 data <- read.csv( "cwurData.csv" )
 #https://www.kaggle.com/mylesoneill/world-university-rankings?select=cwurData.csv
 
+
+
+data <- read.csv("universities_V1.1.csv")
+
+
 data <- data[-(11)] # dane z NA lata 2012 - 2013
 
 filter(data, data$national_rank==229)
